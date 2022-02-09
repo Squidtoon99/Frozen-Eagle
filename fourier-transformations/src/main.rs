30fe0bdd0e71de6ed7d7e022977f9bcdc17ce21f
Squidtoon99 <49101235+Squidtoon99@users.noreply.github.com>
2022-02-09 11:33:48 -0600

    Adding better error handling

--- fourier-transformations/src/main.rs
+++ fourier-transformations/src/main.rs
@@ -20,13 +20,13 @@ fn format_radix(mut x: u32, radix: u32) -> String {
 fn main() {
     let mut line = String::new();
 
-    io::stdin().read_line(&mut line);
+    io::stdin().read_line(&mut line).expect("Read Error");
 
     let t = line.trim().parse::<i32>().unwrap();
 
     for _ in 0..t {
         let mut line = String::new();
-        io::stdin().read_line(&mut line);
+        io::stdin().read_line(&mut line).expect("Read error");
         let n = line.trim().parse::<u32>().unwrap();
         let best_radix = (2..=36).max_by_key(|&radix| {
             format_radix(n, radix).matches("4").count()
